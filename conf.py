89638a3a7a77bc6d7e0244ea0589911c0243cee4
swethavara <79441012+swethavara@users.noreply.github.com>
2021-02-23 16:45:00 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'workshop'
+copyright = '2021, swetha'
+author = 'swetha'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
